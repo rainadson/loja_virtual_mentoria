94381fa58482c1ce252c3d0702600f633b956af6
raina <raina@192.168.0.6>
2024-05-01 18:46:30 -0300

    Commit Classe CategoriaProduto

--- loja_virtual_mentoria/src/main/java/com/example/demo/model/MarcaProduto.java
+++ loja_virtual_mentoria/src/main/java/com/example/demo/model/MarcaProduto.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +20,8 @@ public class MarcaProduto implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
-
+	
+	@Column(nullable=false)
 	private String nomeDesc;
 
 	public Long getId() {
